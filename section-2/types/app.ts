1e3b84fec85eaf66ccfd68277d3ae9a9b4935067
Muhammad-Zohaib-Malik <muhammadzohaibmalik10@gmail.com>
2026-06-13 19:22:52 +0500

    Add foundational TypeScript documentation and examples on types and tooling

---
+++ section-2/types/app.ts
@@ -0,0 +1,24 @@
+// let a:any=5
+// let b:unknown="hey"
+
+
+// if(typeof b ==="number"){
+//    console.log(b.toFixed())
+// }
+
+
+// if(typeof b ==="string"){
+//    console.log(b.toUpperCase())
+// }
+
+
+let a=5
+
+if(typeof a==="number")
+{
+    console.log(a)
+}
+else {
+    let b:string=a
+    console.log(b)
+}
