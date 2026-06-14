e864fb71a773fc4981489cb5faa52ece4c6adcb1
Muhammad-Zohaib-Malik <muhammadzohaibmalik10@gmail.com>
2026-06-14 22:36:52 +0500

    Add initial TypeScript files for arrays, literal types, and type aliases

---
+++ section-4/literal-type/app.ts
@@ -0,0 +1,14 @@
+
+// // let a:"Apple" | "Papaya" | "Mango" | "Grapes"="Apple"
+
+// type AvailableFruit="Apple" | "Papaya" | "Mango" | "Grapes"
+// type DiceNumber=1 | 2 | 3 | 4 | 5 |6
+
+
+// let a:AvailableFruit="Apple"
+
+// let myDiceValie:DiceNumber=6
+
+
+
+let a="Hi"
---
+++ section-4/type-alias/app.ts
@@ -0,0 +1,4 @@
+type User=string | number | boolean
+
+let a:User[]=['zohaib',23,true]
+
